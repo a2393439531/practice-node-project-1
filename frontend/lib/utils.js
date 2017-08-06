dcb88aed9cc589972dfbf59abb66f131e53024b3
wangjian <jian.wang@hpe-china.com>
2017-08-07 06:09:27 +0800

    markdown

--- frontend/lib/utils.js
+++ frontend/lib/utils.js
@@ -1,11 +1,16 @@
 import marked from 'marked';
-
+import Highlight from 'highlight.js';
 import xss from 'xss';
 
 export function redirectURL(url) {
   location = url;
 }
 
+marked.setOptions({
+  highlight: function (code) {
+    return Highlight.highlightAuto(code).value;
+  }
+});
 
 const xssOptions = {
   whiteList: Object.assign({}, xss.whiteList),
@@ -13,3 +18,7 @@ const xssOptions = {
 xssOptions.whiteList.code = ['class'];
 xssOptions.whiteList.span = ['class'];
 const myxss = new xss.FilterXSS(xssOptions);
+
+export function renderMarkdown(text) {
+  return myxss.process(marked(text));
+}
